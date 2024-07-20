f797e16898c7511713bfd588c729559d9f942a5f
laleeroy <gabwielcastwo@gmail.com>
2024-07-20 22:35:24 +0800

    Initial Commit
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

---
+++ archive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the tag name from the environment variable
+REPO_TAG_NAME=$1
+
+# Define the output zip file name
+ZIP_FILE="8BP-$REPO_TAG_NAME.zip"
+
+# Find all files in the current directory except shell scripta and the git related directories, and create a zip file
+zip -r "$ZIP_FILE" . -x "*.sh" -x ".git/*" -x ".github/*"
+
+echo "Files have been zipped into $ZIP_FILE, excluding all shell scripts and the git related directories."
