2a66bd2a00887cd622d60bb19651c44e66afc9ed
laleeroy <gabwielcastwo@gmail.com>
2024-10-26 14:32:36 +0800

    Ignore readme
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- archive.sh
+++ archive.sh
@@ -7,6 +7,6 @@ REPO_TAG_NAME=$1
 ZIP_FILE="8BP-$REPO_TAG_NAME.zip"
 
 # Find all files in the current directory except shell scripta and the git related directories, and create a zip file
-zip -r "$ZIP_FILE" . -x "*.sh" -x ".git/*" -x ".github/*" -x "switch/tinfoil/icons.db"
+zip -r "$ZIP_FILE" . -x "*.sh" -x ".git/*" -x ".github/*" -x "switch/tinfoil/icons.db" -x "README.md"
 
 echo "Files have been zipped into $ZIP_FILE, excluding all shell scripts and the git related directories."
